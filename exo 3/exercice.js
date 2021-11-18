8efe483147e46cf9fa5ac174991db75736252b7b
Noah2211 <noah.decroix3@gmail.com>
2021-11-18 09:33:44 +0100

    Exercice3 done (full exercice done)

--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,6 +1,9 @@
+let divClassName = document.getElementsByClassName("name-tag")
 
+for (i = 0; i < divClassName.length; i++){
 
-
+    divClassName[i].innerHTML = document.createElement("p").innerText = "HELLO WORLD !"
+}
 
 
 
